7da0aa5949d889d9a73d82e4794062ab91084b7a
Evert-Jan van Dijk <evert-janvandijk@hotmail.com>
2019-03-26 21:46:40 +0100

    Add feature to make drafts and publish them.

--- blog/views.py
+++ blog/views.py
@@ -24,9 +24,8 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            # the publisher = the current ,logged in, user
+            # the poster = the current ,logged in, user
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             # show result immedeatly after saving the form
             return redirect('post_detail', pk=post.pk)
@@ -45,7 +44,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -53,4 +51,13 @@ def post_edit(request, pk):
         context_dictionary = {'form': form}
     return render(request, 'blog/post_edit.html', context_dictionary)
 
-# TIMESTAMP: 8:00
+def post_draft_list(request):
+    # show all of the drafts in decending order:
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    context_dictionary = {'posts': posts}
+    return render(request, 'blog/post_draft_list.html', context_dictionary)
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
